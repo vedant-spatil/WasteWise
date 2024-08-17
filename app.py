935439e32d9322fa9191fcdfdfc696280a5a53fb
Vedant <vedantsudhirpatil@gmail.com>
2024-08-17 21:34:12 +0530

    app.py updated

--- app.py
+++ app.py
@@ -1,3 +1,7 @@
+import torch
+import numpy as np
+from time import time
+from PIL import Image 
 import tempfile
 import cv2
 import streamlit as st
@@ -9,6 +13,39 @@ def main():
 
     #Functions
 
+    # def __init__(self, capture_index):
+    #     self.capture_index = capture_index
+    #     self.device = 'cuda' if torch.cuda.is_available() else 'cpu'
+    #     print("Using Device: ", self.device)
+
+    def predictTrash(model, source, save_img, confidence):
+        prediction = model.predict(source=source, save=save_img, conf=confidence)
+        return prediction
+
+    def count(results):
+        for result in results:
+            box = result.boxes
+        return len(box)
+
+    def draw_bounding_boxes(image, results):
+        #Function to draw bounding boxes on the image based on the YOLO output.
+        for result in results:
+            if result.boxes is not None:
+                for box in result.boxes:
+                    # Extract the bounding box coordinates
+                    x1, y1, x2, y2 = map(int, box.xyxy[0].tolist())  # Convert to integer
+                    label = result.names[int(box.cls[0])]  # Get the label using the class index
+                    confidence = box.conf[0].item()  # Get the confidence score
+                    
+                    # Draw the bounding box
+                    cv2.rectangle(image, (x1, y1), (x2, y2), color=(0, 255, 0), thickness=2)
+                    
+                    # Draw the label and confidence
+                    cv2.putText(image, f"{label} {confidence:.2f}", (x1, y1 - 10), 
+                    cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
+
+        return image
+          
     def home_page():
         
         st.markdown(
@@ -17,15 +54,21 @@ def main():
             .big-font {
                 font-size:60px !important;
                 line-height:1.3;
-                padding-bottom:15px;
                 padding-left:15px;
             }
             .medium-font {
                 font-size:25px !important;
                 color:grey;
                 line-height:1.3;
-                padding-bottom:15px;
+                padding-bottom:0px;
                 padding-left:15px;
+            }
+            .buttons-div{
+                display:flex;
+                align-items:center;
+                # justify-content:center;
+                justify-content:flex-start;
+                gap:1vw;
             }
             .but {
                 margin-left:15px;
@@ -51,50 +94,59 @@ def main():
             st.markdown('<p class="big-font">Empowering Waste Management with <b>Waste Wise</b></p>', unsafe_allow_html=True)
             st.markdown('<p class="medium-font">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </p>', unsafe_allow_html=True)
             #btn1=st.button(label="Get Started")
-            st.markdown('<button class="but">Get Started</button>', unsafe_allow_html=True)
+            st.markdown(
+                """
+                <div class="buttons-div">
+                <button class="but">Get Started</button>
+                <button class="but">Learn More</button>
+                </div>
+                """, 
+                unsafe_allow_html=True
+                )
+
         with col2:
             st.image(img,width=400)
     
     def test_page():
-        st.title("Test")
-        st.write("Welcome to the Test page of the app!")
-
-        #Title
-
-        st.title('WasteWise')
-
+        
         #Sidebar-------------------------------------------------------------
+        with st.sidebar:
+            
+            #Select Model
+            st.title('Model')
+            assigned_model_display_name = st.selectbox('Select The Model', list(model_list.keys()))
+            model = model_list[assigned_model_display_name]
+            assigned_model_id = list(model_list.keys()).index(assigned_model_display_name)
 
-        st.sidebar.title('Model')
-        assigned_model_id = []
-        assigned_model = st.sidebar.selectbox('Select The Model', list(model))
-        assigned_model_id.append(model.index(assigned_model))
+            st.markdown('---')
 
-        st.sidebar.markdown('---')
-        st.sidebar.title('Settings')
+            #Settings
+            st.title('Settings')
 
-        st.markdown(
-            """
-            <style>
-            [data-testid="stSidebar"][aria-expanded="true]> div:fist-child{width: 400px;}
-            [data-testid="stSidebar"][aria-expanded="true]> div:fist-child{width: 400px; margin-left: -400px}
-            </style>
-            """,
-            unsafe_allow_html = True,
-        )
-        
-        confidence = st.sidebar.slider('Confidence', min_value=0.0, max_value=1.0, value=0.25)
+            st.markdown(
+                """
+                <style>
+                [data-testid="stSidebar"][aria-expanded="true"]> div:fist-child{width: 400px;}
+                [data-testid="stSidebar"][aria-expanded="true"]> div:fist-child{width: 400px; margin-left: -400px}
+                </style>
+                """,
+                unsafe_allow_html = True,
+            )
+            
+            confidence = st.slider('Confidence', min_value=0.0, max_value=1.0, value=0.25)
 
-        save_img = st.sidebar.checkbox('Save Output')
-        enable_GPU = st.sidebar.checkbox('Enable GPU')
-        custom_classes = st.sidebar.checkbox('Use Custom Classes')
-        
-        assigned_class_id = []
-        if custom_classes:
-            assigned_class = st.sidebar.multiselect('Select The Custom Classes', list(names), default='Plastic')
-            for each in assigned_class:
-                assigned_class_id.append(names.index(each))
-        
+            save_img = st.checkbox('Save Output')
+            enable_GPU = st.checkbox('Enable GPU')
+            custom_classes = st.checkbox('Use Custom Classes')
+            
+            assigned_class_id = []
+            if custom_classes:
+                assigned_class = st.multiselect('Select The Custom Classes', list(names), default='Plastic')
+                for each in assigned_class:
+                    assigned_class_id.append(names.index(each))
+            
+            #start = st.button('Test Model')
+            
         # video_file_buffer = st.sidebar.file_uploader('Upload a video', type= ['mp4', 'mov', 'avi', 'asf', 'm4v'])
         # DEMO_VIDEO='D:/WasteWise/Assets/video.mp4'
         # tffile = tempfile.NamedTemporaryFile(suffix='.mp4', delete=False)
@@ -132,32 +184,137 @@ def main():
         # with kpi3:
         #     st.markdown("**Width**")
         #     kpi3_text= st.markdown(0)
-        #Model
 
-        trashModel = YOLO('Models/garbClass_25epochs.pt')
-        waterModel = YOLO('Models/waterTrash_25epochs.pt')
+        #Main Page-----------------------------------------------------------
 
-        def predictTrash(source, save_img, confidence):
-            prediction = trashModel.predict(source=source, save=save_img, conf=confidence)
-            return prediction
+        #Select the medium
+        assigned_source_id = []
+        assigned_source = st.selectbox('Select The Source', list(sources))
+        assigned_source_id.append(sources.index(assigned_source))
 
-        def count(results):
-            for result in results:
-                box = result.boxes
-            return len(box)
+         #Upload file
+        uploaded_file = st.file_uploader("Upload an image...", type=["jpg", "jpeg", "png"])
+        if uploaded_file is not None:
+            image = Image.open(uploaded_file)
+            image = np.array(image)
+
+            result = predictTrash(model, image, save_img, confidence)
+
+            image_with_boxes = draw_bounding_boxes(image, result)
+
+            st.image(image_with_boxes, caption='Processed Image', use_column_width=True)
+            
+            n = count(result)
+            st.markdown(f"Total objects detected: **{n}**")
         
-        # Main Page
-
-        DEMO_IMG = 'D:/WasteWise/Assets/trash_bottle.jpg'
-        result = predictTrash(DEMO_IMG, save_img, confidence)
-        n = count(result)
-        st.markdown(n)
-
     def about_page():
-        st.title("About")
-        st.write("Welcome to the About page of the app!")
+        st.markdown(
+        """
+        <style>
+            .wrapper{
+                display: flex;
+                flex-direction: column;
+                justify-content: center;
+                margin-top: 20px;
+                margin-bottom: 70px;
+                padding-left: 10px;
+                padding-right: 10px;
+            }
+            .wrapper-center{
+                display: flex;
+                flex-direction: column;
+                justify-content: center;
+                margin-top: 20px;
+                margin-bottom: 70px;
+                padding-left: 10px;
+                padding-right: 10px;
+                align-items:center;
+                text-align:center;
+            }
+            .big-font {
+                font-size:60px !important;
+                line-height:1.1;
+                padding-left:15px;
+            }
+            .medium-font {
+                font-size:25px !important;
+                color:grey;
+                line-height:1.3;
+                padding-bottom:0px;
+                padding-left:15px;
+            }
+        </style>
+        """,
+        unsafe_allow_html=True,
+        )
+
+        st.markdown(
+            """
+            <div class="wrapper">
+                <p class="big-font">Why is WasteWise essential for modern waste management?</p>
+                <p class="medium-font">WasteWise is crucial for modern waste management as it provides an innovative solution to one of the most persistent challenges — effective waste segregation. By accurately detecting and classifying different types of waste, WasteWise simplifies the process of separating recyclable materials from general waste, making collection and disposal more efficient. This not only benefits the environment but also assists workers who struggle to identify and sort waste manually, reducing the margin for error and enhancing their safety. WasteWise can be seamlessly integrated into household waste management, ensuring that organic, recyclable, and hazardous materials are correctly sorted at the source, thereby streamlining the recycling process. Additionally, its application extends to underwater environments, where it aids in the detection and segregation of aquatic waste, a crucial step in combating water pollution and preserving marine ecosystems. By addressing these multifaceted issues, WasteWise emerges as an indispensable tool in our collective efforts toward sustainable waste management.</p>
+            </div>
+            <div class="wrapper-center">
+                <p class="big-font">See WasteWise in Action</p> 
+            </div>
+            """,
+            unsafe_allow_html=True,
+            )
 
     def contact_page():
+        #Assigning bootstap icons to the page
+        st.markdown(
+            """
+            <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons/font/bootstrap-icons.css" rel="stylesheet">
+            """,
+            unsafe_allow_html=True,
+        )
+
+        #Sidebar------------------------
+        st.markdown(
+            """
+            <style>
+            [data-testid="stSidebar"][aria-expanded="true"]>{width: 900px;}
+            [data-testid="stSidebar"][aria-expanded="true"]>{width: 900px; margin-left: -400px}
+            </style>
+            """,
+            unsafe_allow_html = True,
+        )
+
+        #Arrow icon
+        st.sidebar.markdown(
+            """
+            <h1 style="text-align: right;"><i class="bi bi-arrow-down-left"></i></h1>
+            <br></br>
+            """,
+            unsafe_allow_html=True,
+        )
+
+        #Contact Details
+        st.sidebar.markdown(
+            """
+            <div>
+            <h4 style="color: #FF4B4B">Contact Details</h4>
+            <h2 style="line-height:0.9">
+                <i class="bi bi-envelope-at"></i><a href="mailto:vedantsudhirpatil.com" style="text-decoration: none; color:#31333F; margin-left:5px;"> vedantsudhirpatil@gmail.com</a><br></br>
+                <i class="bi bi-telephone"></i><a href="tel: +919540386773" style="text-decoration: none; color:#31333F; margin-left:5px;"> +91 9540386773</a>
+            </h2>
+            <br></br>
+            <div>
+            <div>
+            <h4 style="color: #FF4B4B">Socials</h4>
+            <h2 style="line-height:0.9">
+                <i class="bi bi-linkedin"></i><a href="https://www.linkedin.com/in/vedantspatil" style="text-decoration: none; color:#31333F; margin-left:10px;"> Linkedin</a><br></br>
+                <i class="bi bi-github"></i><a href="https://github.com/Vedant-SPatil" style="text-decoration: none; color:#31333F; margin-left:10px;"> Github</a><br></br>
+                <i class="bi bi-twitter-x"></i><a href="https://twitter.com/Vedant_SPatil" style="text-decoration: none; color:#31333F; margin-left:10px;"> Twitter</a><br></br>
+                <i class="bi bi-instagram"></i><a href="https://www.instagram.com/vedant_spatil/" style="text-decoration: none; color:#31333F; margin-left:10px;"> Instagram</a>
+            </h2>
+            <div>
+            """,
+            unsafe_allow_html=True,
+        )
+
+        #Main Page
         st.title("Contact")
         st.write("Welcome to the Contact page of the app!")
     
@@ -182,7 +339,15 @@ def main():
 
     #Streamlit (Information Arrays)
     names = ['Biodegradable', 'Cardboard', 'Glass', 'Metal', 'Paper', 'Plastic']
-    model = ['Garbage Detection', 'Water Trash Detection']
+    trashModel = YOLO('Models/garbClass_25epochs.pt')
+    waterModel = YOLO('Models/waterTrash_25epochs.pt')
+    sources=['Image','Video','Webcam']
+    model_list = {
+        'Garbage Detection': trashModel,
+        'Water Trash Detection': waterModel
+    }
+    DEMO_IMG = 'D:/WasteWise/Assets/trash_bottle.jpg'
+
 
     #Navbar
     selected = option_menu(
